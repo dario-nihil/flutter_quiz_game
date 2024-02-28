183139898dfc82f88833b4bf7d7f4167dcc21e29
dario-nihil <dario.nihil@gmail.com>
2024-02-28 16:02:13 +0100

    added google-font package and changed fontFamily to the text of QuestionScreen & StartScreen widgets

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:quiz_game/answer_button.dart';
 import 'package:quiz_game/data/questions.dart';
@@ -35,7 +36,12 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(color: Colors.white),
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 201, 153, 251),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
             ),
             const SizedBox(
               height: 30,
